7285f825ce59c501ab025d3dea3727b74a41d6cc
Patrick Wozniak <email@patwoz.de>
2017-10-08 01:37:20 +0200

    added prop-types

--- app/index.js
+++ app/index.js
@@ -6,23 +6,41 @@
 
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
+import PropTypes from 'prop-types';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
-});
+const propTypes = {
+  headline: PropTypes.string.isRequired,
+  editText: PropTypes.string.isRequired,
+  instructions: PropTypes.string.isRequired,
+};
 
-export default class App extends Component {
+const WelcomeMessage = ({ headline, editText, instructions }) => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>{headline}</Text>
+    <Text style={styles.instructions}>{editText}</Text>
+    <Text style={styles.instructions}>{instructions}</Text>
+  </View>
+);
+
+WelcomeMessage.propTypes = propTypes;
+
+class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <WelcomeMessage
+        headline="Welcome to React Native!"
+        editText="To get started, edit App.js"
+        instructions={Platform.select({
+          ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+          android:
+            'Double tap R on your keyboard to reload,\n' +
+            'Shake or press menu button for dev menu',
+        })}
+      />
     );
   }
 }
+export default App;
 
 const styles = StyleSheet.create({
   container: {
