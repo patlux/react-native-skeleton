a68fd73aefff33259787ee716d4d69ce38a33835
Patrick Wozniak <email@patwoz.de>
2017-11-02 14:22:32 +0100

    added idx

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@
  * @flow
  */
 
+import idx from 'idx';
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
@@ -24,18 +25,25 @@ const WelcomeMessage = ({ headline, editText, instructions }) => (
 
 WelcomeMessage.propTypes = propTypes;
 
+const MESSAGES = {
+  WELCOME: {
+    HEADLINE: 'Welcome to React Native!',
+    EDIT_TEXT: 'To get started, edit App.js',
+    INSTRUCTIONS: {
+      ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+      android:
+        'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+    },
+  },
+};
+
 class App extends Component {
   render() {
     return (
       <WelcomeMessage
-        headline="Welcome to React Native!"
-        editText="To get started, edit App.js"
-        instructions={Platform.select({
-          ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-          android:
-            'Double tap R on your keyboard to reload,\n' +
-            'Shake or press menu button for dev menu',
-        })}
+        headline={idx(MESSAGES, _ => _.WELCOME.HEADLINE)}
+        editText={idx(MESSAGES, _ => _.WELCOME.EDIT_TEXT)}
+        instructions={Platform.select(idx(MESSAGES, _ => _.WELCOME.INSTRUCTIONS))}
       />
     );
   }
